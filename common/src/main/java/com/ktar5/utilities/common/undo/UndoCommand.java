3f527e35ea3c0139d3c0668dc22302501da4f0c9
Carter Gale <ktarfive@gmail.com>
2019-05-31 12:52:06 -0700

    Fixed all known Undo bugs

--- common/src/main/java/com/ktar5/utilities/common/undo/UndoCommand.java
+++ common/src/main/java/com/ktar5/utilities/common/undo/UndoCommand.java
@@ -1,6 +1,13 @@
 package com.ktar5.utilities.common.undo;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
 public abstract class UndoCommand {
+    @Getter(AccessLevel.PACKAGE)
+    @Setter(AccessLevel.PACKAGE)
+    private UndoCommand next, previous;
 
     /**
      * This also is do. This is called when added to the stack.
--- common/src/main/java/com/ktar5/utilities/common/undo/UndoStack.java
+++ common/src/main/java/com/ktar5/utilities/common/undo/UndoStack.java
@@ -1,82 +1,62 @@
 package com.ktar5.utilities.common.undo;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class UndoStack {
-    private final List<UndoCommand> undoCommands;
-    //Note that cursor is stored in amount backwards from undoCommands.size()
-    private int cursor = 0;
+    private boolean lastUndo;
 
-    private boolean allUndone, allRedone;
+    //Note on cursor:
+    // The cursor will ALWAYS BE VISIBLE
+    // **UNLESS** IT IS THE TAIL
+    private UndoCommand latest, oldest, cursor;
 
     //TODO limit the list to a certain number of values
-    public UndoStack() {
-        undoCommands = new ArrayList<>();
-    }
 
     public void push(UndoCommand command) {
         if (!(command instanceof UndoGroup)) {
             command.redo();
         }
-        if (cursor != 0) {
-            while (cursor > 0) {
-                undoCommands.remove(undoCommands.size() - 1);
-                cursor--;
-            }
-            cursor = 0;
-        }
-        undoCommands.add(command);
-        allUndone = false;
-        allRedone = true;
-//        System.out.println("Added; size: " + undoCommands.size() + " cursor: " + cursor);
-    }
-
-    /*
-    NOTE: IM NOT ENTIRELY SURE HOW I GOT THIS TO WORK BUT IT DOES SO PLEASE DON'T TOUCH IT
-     */
-
-    public void redo() {
-//        System.out.println("Redo method start");
-        if (allRedone || undoCommands.size() == 0) {
-//            System.out.println("Was last redo, don't redo. Cursor: " + cursor + ". Size: " + undoCommands.size());
+        if (oldest == null || lastUndo) {
+            oldest = latest = cursor = command;
+            lastUndo = false;
             return;
         }
-//        System.out.println("Pre-Redo. Cursor: " + cursor + ". Index: " + (undoCommands.size() - cursor - 1) + "Size: " + undoCommands.size());
-        if (allUndone) {
-            //Don't decrement cursor, because of the double 0 problem (index not ever being -1)
-        } else if(cursor > 0) {
-            cursor -= 1;
-        } else {
-            //Index still at max value (all redone)
-            allRedone = true;
-//            System.out.println("All are now redone");
+
+        if (cursor != latest) {
+            cursor.setNext(null);
+            latest = cursor;
         }
-        undoCommands.get(undoCommands.size() - cursor - 1).redo();
 
+        latest.setNext(command);
+        command.setPrevious(latest);
 
-        allUndone = false;
+        latest = command;
 
-//        System.out.println("Post-Redo. Cursor: " + cursor + ". Index: " + (undoCommands.size() - cursor - 1) + "Size: " + undoCommands.size());
+        cursor = command;
+    }
+
+    public void redo() {
+        if (lastUndo) {
+            cursor.redo();
+            lastUndo = false;
+            return;
+        }
+
+        if (cursor.getNext() != null) { //Cursor is not the latest (should never have to redo the last one) (because cursor is always visible)
+            cursor = cursor.getNext();
+            cursor.redo();
+        }
     }
 
     public void undo() {
-        allRedone = false;
-//        System.out.println("Undo method start");
-        if (allUndone || undoCommands.size() == 0) {
-//            System.out.println("Was last UNDO, don't UNDO. Cursor: " + cursor + ". Size: " + undoCommands.size());
+        if (lastUndo) {
             return;
         }
-        int indexOfCursor = undoCommands.size() - cursor - 1;
-//        System.out.println("Pre-UNdo. Cursor: " + cursor + ". Index: " + indexOfCursor + "Size: " + undoCommands.size());
-        undoCommands.get(indexOfCursor).undo();
-        if (indexOfCursor != 0) {
-            cursor += 1;
+
+        cursor.undo();
+        if (cursor == oldest) {
+            lastUndo = true;
         } else {
-//            System.out.println("All are now undone");
-            allUndone = true;
+            cursor = cursor.getPrevious();
         }
-//        System.out.println("Post-UNdo. Cursor: " + cursor + ". Index: " + (undoCommands.size() - cursor - 1) + "Size: " + undoCommands.size());
     }
 
 
